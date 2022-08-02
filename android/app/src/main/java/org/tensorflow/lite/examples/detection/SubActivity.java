1f2dbae97eb62edcd79d0306726c3af14aaaf9c3
yekwon <rkd5562@naver.com>
2022-08-02 17:45:21 +0900

    jsoup

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/SubActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/SubActivity.java
@@ -4,19 +4,70 @@ package org.tensorflow.lite.examples.detection;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SubActivity extends Activity {
 
+    private String drwNoUrl = "https://dhlottery.co.kr/gameResult.do?method=byWin";
+    private TextView textviewHtmlDocument;
+    private String htmlContentInStringFormat = "";
+    private String nowCnt = "" ;
+    String TAG = "Lotto" ;
     public ListView foodView;
     ArrayList<String> foodStrings = new ArrayList<String>();
     Button btn2;
     Button btn3;
+
+    private class JsoupAsyncTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            Log.i(TAG, Arrays.toString(params));
+            try {
+                // 파라미터로 넘어온 값을 이용해서 url 와 회차를 설정한다.
+                String callUrl = params[0] + "&drwNo=" + params[1];
+                Document doc = Jsoup.connect(callUrl).get();
+                // 위의 html tag에서 결과숫자를 싸고 있는 span tag 을 class명을 이용함.
+                Elements links = doc.select(".ball_645");
+                Log.e(TAG, "links=" + links.size());
+                htmlContentInStringFormat += "회차=" + nowCnt ;
+                for(Element el : links) {
+                    Log.e(TAG, el.text()) ;
+                    htmlContentInStringFormat += " " + el.text() ;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textviewHtmlDocument.setText(htmlContentInStringFormat);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -25,7 +76,7 @@ public class SubActivity extends Activity {
 
         btn2 = findViewById(R.id.btn2);
         final Bundle bundle = new Bundle();
-        foodView = findViewById(R.id.food_list);
+//        foodView = findViewById(R.id.food_list);
 //    textView = (TextView) findViewById(R.id.rec_food);
 
         btn3 = findViewById(R.id.btn3);
@@ -49,6 +100,22 @@ public class SubActivity extends Activity {
                 startActivity(intents);
             }
         });
+        textviewHtmlDocument = (TextView) findViewById(R.id.textView);
+        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod());
 
+        Button htmlTitleButton = (Button) findViewById(R.id.button2);
+        htmlTitleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nowCnt = "969"; // 2021.06.30 기준 마지막 회차
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute(drwNoUrl, nowCnt);
+
+            }
+        });
     }
+//    여기부터는 이제 만드는중 /// 운동 종류 jsoup
+//    1
+
 }

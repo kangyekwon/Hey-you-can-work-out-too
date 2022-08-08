221e1798ca42647ccfc96aedc96b06fbb9551b0b
이준한 <54476630+LEEJUNHAN9305@users.noreply.github.com>
2022-08-08 14:33:23 +0900

    front_page completed & SubActivity complemented

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/FrontActivity.java
@@ -0,0 +1,29 @@
+package org.tensorflow.lite.examples.detection;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+
+public class FrontActivity extends Activity {
+    Button btn_to_activity;
+    ImageView front;
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front);
+        btn_to_activity = findViewById(R.id.to_activity);
+        front = (ImageView)findViewById(R.id.front);
+        front.setImageResource(R.drawable.front);
+        btn_to_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DetectorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

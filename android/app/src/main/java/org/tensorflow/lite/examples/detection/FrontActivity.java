a5e5eedc2d1ce86c4a26478a5d7d0964944d51c8
yekwon <rkd5562@naver.com>
2022-08-22 17:05:57 +0900

    finally!!!!

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/FrontActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/FrontActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 public class FrontActivity extends Activity {
     Button btn_to_activity;
     ImageView front;
+    ImageView front3;
     // BackPressHandler 객체 선언, 할당
     private BackPressHandler backPressHandler = new BackPressHandler(this);
     @Override
@@ -18,14 +19,22 @@ public class FrontActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front);
         btn_to_activity = findViewById(R.id.to_activity);
+
         front = (ImageView)findViewById(R.id.front);
         front.setImageResource(R.drawable.icon_front_02);
+
+        front3 = (ImageView)findViewById(R.id.front3);
+        front3.setImageResource(R.drawable.title01);
+
+
         btn_to_activity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), DetectorActivity.class);
                 startActivity(intent);
             }
+
+
         });
 
     }
